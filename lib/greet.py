6604ba156a45b330485ef74f450f9b837aa8caab
Vinay Raghavan <svinay@zeomega.com>
2013-10-15 15:55:41 +0530

    Changed to add dummy source

--- lib/greet.py
+++ lib/greet.py
@@ -1,2 +1,20 @@
-'Greeting'
-print 'Hello Python world!'
+#!/usr/bin/env python
+
+#This is a silly greeter
+
+class SillyGreeter (object):
+    '''
+    Silly greeter class
+    '''
+    __greetMsg = "DEFAULT GREET MSG"
+    def __init__ (self, msg):
+        if msg:
+            self.__greetMsg = msg
+
+    def ret_greet(self):
+        return self.__greetMsg
+
+if __name__ == '__main__':
+    s = SillyGreeter(" HOLA! ")
+    print s.ret_greet()
+
